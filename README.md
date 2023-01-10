15a79ef9bb2f253d6420e8656e68e27675a348f0
Cristiano Sellitto <99354780+CristianoSellitto@users.noreply.github.com>
2023-01-10 10:12:57 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ICS3U-Template-CPP
+
+[![Mr Coxall's Super Linter](https://github.com/CristianoSellitto/ICS3U-Template-CPP/workflows/Mr%20Coxall's%20Super%20Linter/badge.svg)](https://github.com/CristianoSellitto/ICS3U-Template-CPP/actions/)
---
+++ temp.cpp
@@ -0,0 +1,17 @@
+// Copyright Cristiano 2022
+//
+// Created by Cristiano Sellitto
+// Created in <September> <2022>
+// Insert General Comment
+
+#include <iostream>
+
+int main() {
+    // Insert Function Comment
+
+    std::cout << "Hello, World!";
+    std::cout << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "Done." << std::endl;
+}
